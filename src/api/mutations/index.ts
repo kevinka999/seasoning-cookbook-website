7e93f1b328c73c7f70b86c5b520c33c5e8d399c6
Kevin Katzer <kevinka999@gmail.com>
2026-01-05 13:44:54 -0300

    fix: index import

--- src/api/mutations/index.ts
+++ src/api/mutations/index.ts
@@ -2,6 +2,6 @@ export * from "./loginUser";
 export * from "./signupUser";
 export * from "./refreshToken";
 export * from "./logoutUser";
-export * from "./createUser";
+export * from "./createSeasoningCookbookUser";
 export * from "./createRecipe";
 export * from "./upvoteRecipe";
